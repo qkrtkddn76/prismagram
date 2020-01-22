52eb8d92eba0d3efb66cfd05170492658ac61b3f
qkrtkddn <qkrtkddn76@naver.com>
2020-01-22 17:52:59 +0900

    Follow/UnFollow done

--- README.md
+++ README.md
@@ -11,8 +11,8 @@ instargram clone with Express+ Prisma+ React and React Native
 - [x] Comment on a photo
 - [x] Search by user
 - [x] Search by location
+- [x] Follow / Unfollow User
 - [] See user profile
-- [] Follow / Unfollow User
 - [] See the full photo
 - [] Edit my profile
 - [] Log in
--- src/api/User/confirmSecret/confirmSecret.js
+++ src/api/User/confirmSecret/confirmSecret.js
@@ -7,8 +7,13 @@ export default {
       const { email, secret } = args;
       const user = await prisma.user({ email });
       if (user.loginSecret === secret) {
-        return  generateToken(user.id);
-         
+        await prisma.updateUser({
+          where: { id: user.id },
+          data: {
+            loginSecret: ""
+          }
+        });
+        return generateToken(user.id);
       } else {
         throw Error("Wrong email/secret conviation");
       }
